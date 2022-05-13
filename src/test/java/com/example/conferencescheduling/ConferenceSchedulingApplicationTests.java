47bc4c312976de25e0f8ecd76ed4d03c6e342fdf
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2022-05-13 15:06:04 +0530

    Initial commit

---
+++ src/test/java/com/example/conferencescheduling/ConferenceSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conferencescheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSchedulingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
