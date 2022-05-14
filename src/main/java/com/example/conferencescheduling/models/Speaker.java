0bd345fdb5a7e41ed23d5c4906223e17ce4177db
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2022-05-14 12:12:57 +0530

    add binary type

--- src/main/java/com/example/conferencescheduling/models/Speaker.java
+++ src/main/java/com/example/conferencescheduling/models/Speaker.java
@@ -1,5 +1,7 @@
 package com.example.conferencescheduling.models;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,18 @@ public class Speaker {
     private String title;
     private String company;
     private String speaker_bio;
+
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
+
+    public byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
+    }
 
     @ManyToMany(mappedBy="speakers")
     private List<Session> sessions;
